5a7bbd5345e27abef601ebb5b293b2a784e485c5
Muhammet DILEK <dilekmuhammet@gmail.com>
2012-01-16 01:29:10 +0200

    Controller edited

--- app/controller/Report.js
+++ app/controller/Report.js
@@ -12,35 +12,7 @@ Ext.define('Pigon.controller.Report', {
 
     models: ['City'],
 
-    views: ['chart.Pie','chart.Index','chart.Tab','chart.Grid','chart.Column','chart.Bar'],
+    views: ['chart.Pie','chart.Index','chart.Tab','chart.Grid','chart.Column','chart.Bar']
 
-    init: function() {
-        this.control({
-            'viewport > userlist dataview': {
-                itemdblclick: this.editUser
-            },
-            'useredit button[action=save]': {
-                click: this.updateUser
-            }
-        });
-    },
-
-    editUser: function(grid, record) {
-        //var edit = Ext.create('AM.view.user.Edit').show();
-
-        //edit.down('form').loadRecord(record);
-    },
-
-    updateUser: function(button) {
-        var win    = button.up('window'),
-            form   = win.down('form'),
-            record = form.getRecord(),
-            values = form.getValues();
-
-        record.set(values);
-        win.close();
-        this.getUsersStore().sync();
-
-    }
 });
 
